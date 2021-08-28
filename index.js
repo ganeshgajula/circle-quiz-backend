a07979ed3992eb36687201753290b12e4d16c97d
Ganesh Gajula <ganeshgajula@Ganeshs-MacBook-Air.local>
2021-08-28 20:10:28 +0530

    feat: db connectivity

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+const initializeDbConnection = require("./db/db.connect");
+
 const app = express();
+app.use(cors());
 
 const PORT = 4000;
+
+initializeDbConnection();
 
 app.get("/", (req, res) => {
   res.send("Welcome to express app");
