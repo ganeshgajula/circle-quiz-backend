8204e3976c0b4a2a96f19ea87a2a0c7c3154e17a
Ganesh Gajula <ganeshgajula@Ganeshs-MacBook-Air.local>
2021-08-28 23:06:02 +0530

    add: quiz model

---
+++ models/quiz.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const QuizSchema = new mongoose.Schema({
+  quizSet: [
+    {
+      quizName: { type: String, required: "quizname is required" },
+      level: { type: String, required: "quiz level must be specified" },
+      coverImage: { type: String, required: "cover image link is required" },
+      questions: [
+        {
+          question: { type: String, required: "quiz must have a question" },
+          points: {
+            type: Number,
+            required: "points for each question must be specified",
+          },
+          negativePoints: {
+            type: Number,
+            required: "negative points for each question must be specified.",
+          },
+          options: [
+            {
+              text: { type: String, required: "option text must be provided" },
+              isCorrect: {
+                type: Boolean,
+                required: "correct answer status must be provided",
+              },
+            },
+          ],
+        },
+      ],
+    },
+  ],
+});
+
+const Quiz = mongoose.model("Quiz", QuizSchema);
+
+module.exports = { Quiz };
