da77eab7c67ada549f76208581130e335d009077
Ganesh Gajula <ganeshgajula@Ganeshs-MacBook-Air.local>
2021-08-29 14:18:24 +0530

    refactor: quiz schema

--- models/quiz.model.js
+++ models/quiz.model.js
@@ -1,31 +1,27 @@
 const mongoose = require("mongoose");
 
 const QuizSchema = new mongoose.Schema({
-  quizSet: [
+  quizName: { type: String, required: "quizname is required" },
+  level: { type: String, required: "quiz level must be specified" },
+  coverImage: { type: String, required: "cover image link is required" },
+  questions: [
     {
-      quizName: { type: String, required: "quizname is required" },
-      level: { type: String, required: "quiz level must be specified" },
-      coverImage: { type: String, required: "cover image link is required" },
-      questions: [
+      question: { type: String, required: "quiz must have a question" },
+      points: {
+        type: Number,
+        required: "points for each question must be specified",
+      },
+      negativePoints: {
+        type: Number,
+        required: "negative points for each question must be specified.",
+      },
+      options: [
         {
-          question: { type: String, required: "quiz must have a question" },
-          points: {
-            type: Number,
-            required: "points for each question must be specified",
+          text: { type: String, required: "option text must be provided" },
+          isCorrect: {
+            type: Boolean,
+            required: "correct answer status must be provided",
           },
-          negativePoints: {
-            type: Number,
-            required: "negative points for each question must be specified.",
-          },
-          options: [
-            {
-              text: { type: String, required: "option text must be provided" },
-              isCorrect: {
-                type: Boolean,
-                required: "correct answer status must be provided",
-              },
-            },
-          ],
         },
       ],
     },
