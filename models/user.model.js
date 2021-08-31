96da78c6cd2050551c19f6c775248e96119206c0
Ganesh Gajula <ganeshgajula@Ganeshs-MacBook-Air.local>
2021-08-31 22:46:18 +0530

    add: user model

---
+++ models/user.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstname: { type: String, required: "first name is required" },
+    lastname: { type: String, require: "last name is required" },
+    email: { type: String, required: "email is required" },
+    password: { type: String, required: "password is required" },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = { User };
