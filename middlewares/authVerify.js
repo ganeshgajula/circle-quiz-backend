b27c4b4d39999b9ff781f8b853324a387e6e1afc
Ganesh Gajula <ganeshgajula@Ganeshs-MacBook-Air.local>
2021-09-11 22:01:04 +0530

    fix: imports

--- middlewares/authVerify.js
+++ middlewares/authVerify.js
@@ -1,11 +1,13 @@
+const jwt = require("jsonwebtoken");
+
 const authVerify = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
-  console.log({ token });
+  console.log("authverify", { token });
   try {
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
     console.log({ decoded });
     req.user = { userId: decoded.userId };
-    next();
+    return next();
   } catch (error) {
     res.status(401).json({
       success: false,
